394e60249daae38d387c1e82a9ed26128d84b76a
mariyalasoumya99 <mariyalasoumya11@gmail.com>
2020-07-10 15:02:02 +0530

    First commit

--- src/main/java/onboardingemployee/crudoperations/controller/EmployeeController.java
+++ src/main/java/onboardingemployee/crudoperations/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 //import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 import javax.validation.Valid;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import onboardingemployee.crudoperations.Repository.EmployeeRepository;
 import onboardingemployee.crudoperations.exception.ResourceNotFoundException;
 import onboardingemployee.crudoperations.model.Employee;
+import onboardingemployee.crudoperations.responsemodel.CustomResponse;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -33,29 +33,48 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 
 	@GetMapping("/employees/{Id}")
-	public ResponseEntity<Employee> getEmployee(@PathVariable(value = "Id") Long Id) {
-		return new ResponseEntity<>(employeeRepository.findById(Id).get(),HttpStatus.OK);
+	public ResponseEntity<CustomResponse> getEmployee(@PathVariable(value = "Id") Long Id) {
+		CustomResponse cr = new CustomResponse();
+  	  	Map<String, Object> metaData = new HashMap<>();
+  	  	try {
+  	  	  	Employee emp = employeeRepository.findById(Id).get();
+  	  	cr.setData(emp);
+      	metaData.put("status", "success");
+      	cr.setMetaData(metaData);
+    	return new ResponseEntity<>(cr,HttpStatus.CREATED); 
+  	  	}
+  	  	catch(NoSuchElementException e)
+  	  	{
+  	  	  metaData.put("status", "failure");
+    	  metaData.put("message", "Employee not found with ID :"+Id);
+    	  cr.setMetaData(metaData);
+    	  return new ResponseEntity<>(cr,HttpStatus.NOT_FOUND);
+  	  	}
 	}
+	
 	@GetMapping("/employees")
 	public ResponseEntity<List<Employee>> getEmployee() {
 		return new ResponseEntity<>(employeeRepository.findAll(),HttpStatus.OK);
 	}
 
-
 	@PostMapping("/employees")
-	public ResponseEntity<Employee> createEmployee(@Valid @RequestBody Employee employee) {
+	public ResponseEntity<CustomResponse> createEmployee(@Valid @RequestBody Employee employee) {
 	      String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
-	      if(employee.getEmailId().matches(regex))
-	      {
-	    	  
-	    	  System.out.println("hi");
-	    	  return new ResponseEntity<>(employeeRepository.save(employee),HttpStatus.CREATED);
+    	  CustomResponse cr = new CustomResponse();
+    	  Map<String, Object> metaData = new HashMap<>();
+	      if(employee.getEmailId().matches(regex)) {
+	      	cr.setData(employeeRepository.save(employee));
+	      	metaData.put("status", "success");
+	      	cr.setMetaData(metaData);
+	    	return new ResponseEntity<>(cr,HttpStatus.CREATED); 
 	      }
-	      else
-	      {
-	    	  System.out.println("hello");
-	    	  return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
-	}
+	      else {
+	    	  metaData.put("status", "failure");
+	    	  metaData.put("message", "Invalid email");
+	    	  cr.setMetaData(metaData);
+	    	  return new ResponseEntity<>(cr,HttpStatus.BAD_REQUEST);
+	      }
+	    	  
 	}
 
 	 @PutMapping("/employees/{id}")
@@ -78,10 +97,11 @@ public class EmployeeController {
 			throws ResourceNotFoundException {
 		Employee employee = employeeRepository.findById(employeeId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
+		
 		employeeRepository.delete(employee);
 		Map<String, Boolean> response = new HashMap<>();
 		response.put("deleted", Boolean.TRUE);
+		
 		return response;
 	}
 
